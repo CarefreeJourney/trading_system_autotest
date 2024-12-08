84946c0999b1a627289b0e913c226c76780631ed
lcl <2094277327@qq.com>
2024-12-08 22:15:02 +0800

    contains和starts-with方法

---
+++ base/HomeBase.py
@@ -0,0 +1,25 @@
+# @Version：
+# @Time：2024/12/8 22:00
+# @Author：ChuliLin
+# @Description：
+class HomeBase:
+    def wallet_switch(self):
+        """
+        首页的钱包开关
+        :return:
+        """
+        return "//span[contains(@class,'el-switch__core')]"
+
+    def logo(self):
+        """
+        进入系统后，首页左上角的logo
+        :return:
+        """
+        return "//div[contains(text(),'二手')]"
+
+    def welcome(self):
+        """
+        首页，欢迎您回来
+        :return:
+        """
+        return "//span[starts-with(text(),'欢迎')]"
