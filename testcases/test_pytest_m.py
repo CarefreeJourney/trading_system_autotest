ccceca011736a87057a20fea415ef82aa757042a
lcl <2094277327@qq.com>
2024-12-17 00:22:22 +0800

    fixture初试，使用和作用范围

--- testcases/test_pytest_m.py
+++ testcases/test_pytest_m.py
@@ -6,23 +6,27 @@ from time import sleep
 import pytest
 from config.driver_config import DriverConfig
 class TestPytestMClass:
+    @pytest.fixture()
+    def driver(self):
+        get_driver = DriverConfig().driver_config()
+        return get_driver
     @pytest.mark.bing
-    def test_open_bing(self):
-        driver = DriverConfig().driver_config()
+    def test_open_bing(self,driver):
+        # driver = DriverConfig().driver_config()
         driver.get("https://cn.bing.com")
         sleep(3)
         driver.quit()
 
     @pytest.mark.baidu
-    def test_open_baidu(self):
-        driver = DriverConfig().driver_config()
+    def test_open_baidu(self,driver):
+        # driver = DriverConfig().driver_config()
         driver.get("https://www.baidu.com")
         sleep(3)
         driver.quit()
 
     @pytest.mark.antDesign
-    def test_open_antDesign(self):
-        driver = DriverConfig().driver_config()
+    def test_open_antDesign(self,driver):
+        # driver = DriverConfig().driver_config()
         print("test_open_antDesign")
         driver.get("https://ant-design.antgroup.com/components/select-cn?locale=zh-CN")
         sleep(3)
