e12bc69f7fadac82f94d6efe5d812f6b9b0ed977
lcl <2094277327@qq.com>
2024-12-21 10:09:43 +0800

    失败重跑插件

---
+++ testcases/test_rerun.py
@@ -0,0 +1,16 @@
+# @Version：
+# @Time：2024/12/21 0:48
+# @Author：ChuliLin
+# @Description：
+import random
+import pytest
+class TestRerun:
+    @pytest.mark.flaky(reruns=3, reruns_delay=1)
+    def test_rerun(self):
+        num=random.randint(1,3)
+        print("num:",num)
+        if num!=1:
+            print("失败")
+            raise Exception("出错了")
+        else:
+            print("成功")
