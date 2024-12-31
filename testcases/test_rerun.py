acf2d0968ae89651673c491a683eabbe7f5370bd
lcl <2094277327@qq.com>
2024-12-31 18:21:32 +0800

    删除不需要的测试用例

--- testcases/test_rerun.py
+++ testcases/test_rerun.py
@@ -5,13 +5,14 @@
 import random
 import pytest
 class TestRerun:
-    @pytest.mark.flaky(reruns=1, reruns_delay=1)
-    def test_rerun(self):
-        num=random.randint(1,3)
-        print("num:",num)
-        if num!=1:
-            print("失败")
-            raise Exception("出错了")
-        else:
-            print("成功")
-        assert "william" not in "william UI"
+    pass
+    # @pytest.mark.flaky(reruns=1, reruns_delay=1)
+    # def test_rerun(self):
+    #     num=random.randint(1,3)
+    #     print("num:",num)
+    #     if num!=1:
+    #         print("失败")
+    #         raise Exception("出错了")
+    #     else:
+    #         print("成功")
+    #     assert "william" not in "william UI"
